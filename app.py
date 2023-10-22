e19a53e8eaeb6e30798aa110e1bf1c49ddff59e8
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2023-10-22 06:36:57 -0500

    imoroved styling

--- app.py
+++ app.py
@@ -5,19 +5,26 @@ from datasets import load_dataset
 from sentence_transformers import SentenceTransformer, util
 
 embedder = SentenceTransformer('all-mpnet-base-v2')
-st.title("iSeBetter : Semantic Transfomer.") 
-  
-message = st.text_area("Issue details in the text area.") 
-  
-if st.button("Analyse the Patterns"):
-    query_embedding = embedder.encode(message, convert_to_tensor=True)
+
+st.title("iSeBetter : Semantic Transformer")
+st.header("Analyzing Patterns in Text")
+
+
+text_input = st.text_area("Enter the issue details below:")
+
+
+if st.button("Analyse the Issues"):
+    # Perform analysis (your existing code)
+    query_embedding = embedder.encode(text_input, convert_to_tensor=True)
     corpus_embeddings = torch.load('saved_corpus.pt')
     corpus_embeddings_name = torch.load('saved_corpus_list.txt')
     cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
     top_results = torch.topk(cos_scores, k=5)
 
-    st.write("Top 5 Matched")
-    
+    # Results presentation
+    st.subheader("Top 5 Matched Results:")
+    result_table = "<table><tr><th>Matched Text</th><th>Score</th></tr>"
     for score, idx in zip(top_results[0], top_results[1]):
-         st.write(corpus_embeddings_name[idx], "(Score: {:.4f})".format(score))
-    
+        result_table += f"<tr><td>{corpus_embeddings_name[idx]}</td><td>{score:.4f}</td></tr>"
+    result_table += "</table>"
+    st.markdown(result_table, unsafe_allow_html=True)
