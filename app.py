fa602337dc9efae1eac688a41ddbaf5e7021c7f1
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2023-10-21 20:52:32 -0500

    Added texfields

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 
+import tensorflow as tf
+import streamlit as st 
+st.title("SmartCluster") 
+  
+message = st.text_area("Input the input details in the text area.") 
+  
+if st.button("Analyse the Patterns"):
+    pass 
